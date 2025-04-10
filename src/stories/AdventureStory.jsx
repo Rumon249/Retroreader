5cdc40323fe7de471e230ae099a74957b6416a1d
Aasha Kodukula <aashakodukula1@gmail.com>
2025-04-10 19:36:07 -0400

    Finished up the pages

--- src/stories/AdventureStory.jsx
+++ src/stories/AdventureStory.jsx
@@ -5,18 +5,33 @@ function AdventureStory() {
 
   const stories = [
     {
-      title: 'The Lost Map',
-      image: 'YOUR_IMAGE_URL_HERE',
+      title: 'Peter Pan',
+      image: 'https://variety.com/wp-content/uploads/2016/04/peter-pan.jpg',
       path: '/story/the-lost-map'
     },
     {
-      title: 'Pirate Pete',
-      image: 'YOUR_IMAGE_URL_HERE',
+      title: 'A Christmas Carol',
+      image: 'https://cdn.prod.website-files.com/63c03e257f43688614b2e63f/63fe527dae944cddd900a2a1_ACC%20-%20Show%20Graphic.opt.webp',
       path: '/story/pirate-pete'
     },
     {
-      title: 'Jungle Explorer',
-      image: 'YOUR_IMAGE_URL_HERE',
+      title: 'Green Eggs and Ham',
+      image: 'https://variety.com/wp-content/uploads/2015/03/green-eggs-and-ham-netflix.jpg?w=980&h=551&crop=1',
+      path: '/story/jungle-explorer'
+    },
+    {
+      title: 'The Rainbow Fish',
+      image: 'https://northsouth.com/wp-content/uploads/2022/06/Der-Regenbogenfisch-1200x560-2-1200x560.jpg',
+      path: '/story/the-lost-map'
+    },
+    {
+      title: 'The Gingerbrea Man',
+      image: 'https://i.ytimg.com/vi/YoQyyB5xvLk/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLB97atfCwHzW63_he1H02HbKrxWEw',
+      path: '/story/pirate-pete'
+    },
+    {
+      title: 'Thumbelina',
+      image: 'https://www.all-about-fairies.com/images/Thumbelina_1.jpg',
       path: '/story/jungle-explorer'
     }
   ];
