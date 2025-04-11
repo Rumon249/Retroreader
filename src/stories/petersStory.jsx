69b494a953c14ee44ecc58b7ebaaee2cec7a6d47
Rumon249 <karimrumon123@gmail.com>
2025-04-11 04:20:17 -0400

    giffy stuff

--- src/stories/petersStory.jsx
+++ src/stories/petersStory.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import AnimatedCharacter from "../components/AnimatedCharacter";
 import Peterpan from "../assets/Peterpan.gif";
+import battle from "../assets/battle.gif"
 import "./books.css";
 
 const usePanPages = () => {
@@ -24,10 +25,21 @@ const usePanPages = () => {
     <p>He whisked them off to {highlight("Neverland")}, a place of pirates, fairies, and adventure.</p>,
     <p>They met the {highlight("Lost Boys")} and soared through the skies with pixie dust and dreams.</p>,
     <p>Captain {highlight("Hook")} and his crew plotted revenge, fearing the tick-tock of the crocodile.</p>,
-    <p>A daring battle broke out, and Peter saved his friends with cleverness and courage.</p>,
-    <p>In the end, Wendy chose home — but {highlight("Peter Pan")} stayed young, forever chasing the stars.</p>,
+    <p>A daring
+    <span
+    className="cursor-pointer text-blue-500 underline"
+    onClick={() => setTrigger(prev => prev + 1)}
+    >
+        &nbsp;{highlight("battle ")}
+    </span>
+    <AnimatedCharacter
+        triggerKey={trigger}
+        imageSrc={battle}
+        alt="Battle"
+    /> broke out, and Peter saved his friends with cleverness and courage.</p>,
+    <p>In the end, Wendy chose home — but Peter Pan stayed young, forever chasing the stars.</p>,
 
-    <AnimatedCharacter triggerKey={trigger} imageSrc={Peterpan} />
+    
   ];
 };
 
